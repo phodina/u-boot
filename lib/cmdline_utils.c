70a0c14eceab20005fd8a2b3e9e2bfe87a1de1e6
Petr Hodina <petr.hodina@protonmail.com>
2026-02-08 20:14:40 +0100

    common: Add command line parameter masking functionality
    Extend cmdline_utils to support masking or replacing parameters in
    command line strings. This is useful for filtering sensitive data or
    modifying boot parameters.
    
    Signed-off-by: Petr Hodina <petr.hodina@protonmail.com>

--- lib/cmdline_utils.c
+++ lib/cmdline_utils.c
@@ -83,3 +83,106 @@ cleanup_error:
 	}
 	return -ENOMEM;
 }
+
+/**
+ * mask_cmdline_params() - Mask or replace selected parameters in a command line
+ */
+char *mask_cmdline_params(const char *cmdline, struct cmdline_mask_param *params, int count)
+{
+	char *result, *temp;
+	const char *src_pos;
+	char *dst_pos;
+	int i, result_len, cmdline_len;
+	const char *param_start, *param_end, *value_start, *value_end;
+	char *key_with_equals;
+	int key_len, old_value_len, new_value_len;
+	bool found_match;
+
+	if (!cmdline || !params || count <= 0)
+		return NULL;
+
+	cmdline_len = strlen(cmdline);
+	result_len = cmdline_len * 2;
+	result = malloc(result_len + 1);
+	if (!result)
+		return NULL;
+
+	src_pos = cmdline;
+	dst_pos = result;
+
+	while (*src_pos) {
+		found_match = false;
+
+		for (i = 0; i < count; i++) {
+			if (!params[i].key)
+				continue;
+
+			key_len = strlen(params[i].key);
+			if (params[i].key[key_len - 1] == '=') {
+				key_with_equals = (char *)params[i].key;
+			} else {
+				key_with_equals = malloc(key_len + 2);
+				if (!key_with_equals) {
+					free(result);
+					return NULL;
+				}
+				strcpy(key_with_equals, params[i].key);
+				strcat(key_with_equals, "=");
+			}
+
+			if (strncmp(src_pos, key_with_equals, strlen(key_with_equals)) == 0 &&
+			    (src_pos == cmdline || *(src_pos - 1) == ' ')) {
+				found_match = true;
+				param_start = src_pos;
+				value_start = src_pos + strlen(key_with_equals);
+
+				value_end = strstr(value_start, " ");
+				if (!value_end)
+					value_end = value_start + strlen(value_start);
+				param_end = value_end;
+
+				old_value_len = value_end - value_start;
+
+				if (!params[i].new_value) {
+					if (*param_end == ' ')
+						src_pos++;
+				} else {
+					strncpy(dst_pos, param_start, strlen(key_with_equals));
+					dst_pos += strlen(key_with_equals);
+
+					new_value_len = strlen(params[i].new_value);
+					strcpy(dst_pos, params[i].new_value);
+					dst_pos += new_value_len;
+				}
+
+				src_pos = param_end;
+
+				if (key_with_equals != params[i].key)
+					free(key_with_equals);
+				break;
+			}
+
+			if (key_with_equals != params[i].key)
+				free(key_with_equals);
+		}
+
+		if (!found_match) {
+			*dst_pos++ = *src_pos++;
+		}
+
+		if (dst_pos - result >= result_len - 10) {
+			int current_pos = dst_pos - result;
+			result_len *= 2;
+			temp = realloc(result, result_len + 1);
+			if (!temp) {
+				free(result);
+				return NULL;
+			}
+			result = temp;
+			dst_pos = result + current_pos;
+		}
+	}
+
+	*dst_pos = '\0';
+	return result;
+}
